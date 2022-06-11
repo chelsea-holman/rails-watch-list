1cfb9ae4ec157f825ee39c4a211a994edf850d7a
Chelsea Holman <chel.holman@gmail.com>
2022-06-11 15:24:00 +0800

    sedded db

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,9 @@
 class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
+
+  validates :movie, uniqueness: { scope: :list,
+                                  message: 'Movie has already been added to this list.' }
+
+  validates :comment, length: { minimum: 6 }
 end
